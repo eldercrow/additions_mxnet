6c5747b90ec7d2e1d93ddae5112ecee367151e96
Hyunjoon Lee <hyunjoon.lee@intel.com>
2017-09-22 18:58:04 +0900

    to be merged.

--- ssd/demo_hypernet.sh
+++ ssd/demo_hypernet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python demo.py \
-    --network hypernetv4 \
-    --prefix ./model/ssd_hypernetv4_384 \
+    --network dilatenetv2 \
+    --prefix ./model/ssd_dilatenetv2_384 \
     --epoch 1000 \
     --images ./data/demo/street.jpg \
     --data-shape 384 \
