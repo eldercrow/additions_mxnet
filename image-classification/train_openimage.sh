8c6aeec80d168b7c0385132636b9d89ed312477f
Hyunjoon <eldercrow@gmail.com>
2017-09-25 11:37:30 +0900

    dilatenetv3 w/ openimage.

--- image-classification/train_openimage.sh
+++ image-classification/train_openimage.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 python train_openimage.py \
-    --data-train /home/hyunjoon/dataset/openimage/rec_classification/train_openimage.rec \
-    --network hypernetv6 \
+    --data-train /home/hyunjoon/dataset/openimage_cls/train_openimage.rec \
+    --network dilatenetv3 \
     --batch-size 192 \
     --image-shape '3,192,192' \
     --optimizer nadam \
     --gpus 0,1 \
     --lr 1e-03 \
     --disp 200 \
-    --load-epoch 49 \
-    --model-prefix ./model/hypernetv6_openimage
+    --model-prefix ./model/dilatenetv3_openimage
+    # --load-epoch 49 \
